8cd1e8e7f493b99c699e8b926131abf4d0b1adaa
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-11-19 20:44:31 +0200

    adds props

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ export default function App() {
 	const [responsible, setResponsible] = useState(responsibleState);
 
 	useEffect(() => {
-		setResponsible({ name: "", group: "" });
+		setResponsible({ name: "Admin", group: "admin" });
 	}, []);
 
 	return (
 		<div>
-			{responsible.group === "" && <Authorization />}
+			{responsible.group === "" && <Authorization setResponsible={setResponsible} />}
 
 			{(responsible.group === "admin" || responsible.group === "foreman" || responsible.group === "driver") && <Profile responsible={responsible} />}
 		</div>
