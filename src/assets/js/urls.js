638b96e7d249cd55f33855a88b1423ef9ad2fc85
Serhii <39593488+PylSerg@users.noreply.github.com>
2022-12-19 18:05:03 +0200

    changes variable name

--- src/assets/js/urls.js
+++ src/assets/js/urls.js
@@ -1,4 +1,4 @@
 export const REPORT_MATERIALS_URL = "https://script.google.com/macros/s/AKfycbw8x4ytMRcrnozptsgMwjbW1iBgJ2jGELixG26Hg_FsnbePDB-mNYZrjIXuNyWcl2MM/exec";
-export const OBJECTS_LIST_URL = "https://script.google.com/macros/s/AKfycbyJWwDtfB3ucWhEOFVt7cFkxZ_gATPMYKMgdF8dcVetD7XhP3hRXh0cXUCVpYZgg5GuPw/exec";
+export const CURRENT_OBJECTS_LIST_URL = "https://script.google.com/macros/s/AKfycbyJWwDtfB3ucWhEOFVt7cFkxZ_gATPMYKMgdF8dcVetD7XhP3hRXh0cXUCVpYZgg5GuPw/exec";
 export const STAGES_LIST_URL = "https://script.google.com/macros/s/AKfycbz9ZJ8CCGvF6dp9wVU3b6-VXmsb50Kmcd5aP6qyZqhlvp3XKu9QyLXa_wiZ46PWJLq0/exec";
 export const MATERIALS_LIST_URL = "https://script.google.com/macros/s/AKfycbzPdb81G4lcQsbEishHmTZL9hoU6Z2zghKRTdX7k_29xhuxlb6DCSC9Ys8Ux6jEddU3/exec";
