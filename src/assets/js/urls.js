5262353381d414a3a24a816845e56cb5405afeea
Serhii <39593488+PylSerg@users.noreply.github.com>
2023-01-11 14:46:10 +0200

    changes urls

--- src/assets/js/urls.js
+++ src/assets/js/urls.js
@@ -1,7 +1,7 @@
 export const REPORT_MATERIALS_URL = "https://script.google.com/macros/s/AKfycbwNnzJR_aCXBlcCfowaX6eWbbUNEWjGBjR-vwd7BnFuIIAnNKRCa32j9LBcy1o3yJGI/exec";
 export const REPORT_ADVANCES_AND_SALARIES_URL = "https://script.google.com/macros/s/AKfycbzaTFgzaHSFyj8czcl8-NRQicfV6aEE9LBza5Z97HLVwO6v3_a3cZiH232xn920ShGm/exec";
 export const REPORT_FUEL_URL = "https://script.google.com/macros/s/AKfycbwmJ-P1vxWB5pr_pS4CjIJHKPYqmqepQqL-H02NqNeih65W4JhZrcz8Ltw3t8ZXB7S4mw/exec";
-export const REPORT_OFFICE_URL = "https://script.google.com/macros/s/AKfycbyKvVokQCO2HK_pYiFXuUe-5TCb56fJWakrUL9-78CWfUOcwuvxwiS5iidyDBm9OsGE/exec";
+export const REPORT_OFFICE_URL = "https://script.google.com/macros/s/AKfycbylRGwyC5V9bQdylqQ5Hf1d_TP6HvMGCzIkyPMNfpZSUEOcBC2rik7BxmDh3QMGbH4l/exec";
 export const CURRENT_OBJECTS_LIST_URL = "https://script.google.com/macros/s/AKfycbyJWwDtfB3ucWhEOFVt7cFkxZ_gATPMYKMgdF8dcVetD7XhP3hRXh0cXUCVpYZgg5GuPw/exec";
 export const STAGES_LIST_URL = "https://script.google.com/macros/s/AKfycbz9ZJ8CCGvF6dp9wVU3b6-VXmsb50Kmcd5aP6qyZqhlvp3XKu9QyLXa_wiZ46PWJLq0/exec";
 export const MATERIALS_LIST_URL = "https://script.google.com/macros/s/AKfycbw9oyeXSFwxR8c0fmXCPS1B-AYOkbq8pWBPznJXNezQD_Oc2zotUUTNUjv8HG0FEMA/exec";
