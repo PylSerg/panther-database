22d56a92b9a28ae1f593be697beb842f5b6cee9f
Serhii <39593488+PylSerg@users.noreply.github.com>
2023-01-17 16:20:17 +0200

    adds fetch reports

--- src/components/ViewReportsBlock.js
+++ src/components/ViewReportsBlock.js
@@ -15,13 +15,70 @@ export default function ViewReportsBlock() {
 		returned: [],
 	});
 
-	console.log(`materials`, materials);
+	const [materialsReports, setMaterialsReports] = useState({
+		confirmed: [],
+		unconfirmed: [],
+		returned: [],
+	});
+
+	// console.log(`materials`, materials);
+	// console.log(`materialsReports`, materialsReports);
 
 	const dispatch = useDispatch();
+
+	// xc.rnd("ViewReportsBlock");
 
 	useEffect(() => {
 		getMaterialsReport();
 	}, []);
+
+	useEffect(() => {
+		const unconfirmedReports = [];
+
+		const previousReport = () => {
+			if (unconfirmedReports.length > 0) {
+				return unconfirmedReports.length - 1;
+			} else {
+				return 0;
+			}
+		};
+
+		let reportSum = 0;
+
+		for (let i = 0; i < materials.unconfirmed.length; i++) {
+			if (i === 0) {
+				reportSum = materials.unconfirmed[i].sum;
+
+				addReport();
+			} else {
+				if (materials.unconfirmed[i].report !== materials.unconfirmed[i - 1].report) {
+					unconfirmedReports[previousReport()] = { ...unconfirmedReports[previousReport()], reportSum };
+
+					reportSum = materials.unconfirmed[i].sum;
+
+					addReport();
+				} else {
+					reportSum += materials.unconfirmed[i].sum;
+				}
+			}
+
+			if (i === materials.unconfirmed.length - 1) unconfirmedReports[previousReport()] = { ...unconfirmedReports[previousReport()], reportSum };
+
+			function addReport() {
+				const report = materials.unconfirmed[i];
+
+				unconfirmedReports.push({
+					reportNumber: report.report,
+					reportDate: report.date,
+					reportTime: report.time,
+					reportType: report.type,
+					reportLabel: report.label,
+				});
+			}
+		}
+
+		setMaterialsReports({ ...materialsReports, unconfirmed: unconfirmedReports });
+	}, [materials]);
 
 	async function getMaterialsReport() {
 		dispatch(showProgress("Завантаження звітів..."));
@@ -31,8 +88,6 @@ export default function ViewReportsBlock() {
 			.then(response => {
 				setMaterials({ ...materials, unconfirmed: response.data });
 				dispatch(hideProgress());
-
-				// xc.l("response.materials", response);
 			})
 			.catch(error => {
 				dispatch(hideProgress());
@@ -42,5 +97,21 @@ export default function ViewReportsBlock() {
 			});
 	}
 
-	return <div></div>;
+	return (
+		<div>
+			{materialsReports.unconfirmed[0]?.reportNumber && (
+				<div>
+					<h1>{materialsReports.unconfirmed[0].reportNumber}</h1>
+
+					<ul>
+						{materialsReports.unconfirmed.map(item => {
+							<li key={item.reportNumber}>
+								Report <b>{item.reportNumber}</b>
+							</li>;
+						})}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 }
