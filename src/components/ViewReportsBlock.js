311c20e31624e43afad9d5d0d5bfe30ba55ba0d6
Serhii <39593488+PylSerg@users.noreply.github.com>
2023-01-18 21:47:19 +0200

    adds reports view

--- src/components/ViewReportsBlock.js
+++ src/components/ViewReportsBlock.js
@@ -1,6 +1,6 @@
 import xc from "../@x-console/x-console";
 
-import { REPORT_MATERIALS_URL } from "../assets/data/urls";
+import { REPORT_UNC_MATERIALS_URL } from "../assets/data/urls";
 
 import { useState, useEffect } from "react";
 
@@ -9,109 +9,142 @@ import { showNotification } from "../redux/features/notificationSlice";
 import { showProgress, hideProgress } from "../redux/features/progressSlice";
 
 export default function ViewReportsBlock() {
-	const [materials, setMaterials] = useState({
-		confirmed: [],
-		unconfirmed: [],
-		returned: [],
-	});
+  const [materials, setMaterials] = useState({
+    confirmed: [],
+    unconfirmed: [],
+    returned: [],
+  });
 
-	const [materialsReports, setMaterialsReports] = useState({
-		confirmed: [],
-		unconfirmed: [],
-		returned: [],
-	});
+  const [materialsReports, setMaterialsReports] = useState({
+    confirmed: [],
+    unconfirmed: [],
+    returned: [],
+  });
 
-	console.log(`materials`, materials);
-	console.log(`materialsReports`, materialsReports);
+  //   console.log(`materials`, materials);
+  // console.log(`materialsReports`, materialsReports);
 
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	// xc.rnd("ViewReportsBlock");
+  // xc.rnd("ViewReportsBlock");
 
-	useEffect(() => {
-		getMaterialsReport();
-	}, []);
+  useEffect(() => {
+    getMaterialsReport();
+  }, []);
 
-	useEffect(() => {
-		const unconfirmedReports = [];
+  useEffect(() => {
+    const unconfirmedReports = [];
 
-		const previousReport = () => {
-			if (unconfirmedReports.length > 0) {
-				return unconfirmedReports.length - 1;
-			} else {
-				return 0;
-			}
-		};
+    const previousReport = () => {
+      if (unconfirmedReports.length > 0) {
+        return unconfirmedReports.length - 1;
+      } else {
+        return 0;
+      }
+    };
 
-		let reportSum = 0;
+    let reportSum = 0;
 
-		for (let i = 0; i < materials.unconfirmed.length; i++) {
-			if (i === 0) {
-				reportSum = materials.unconfirmed[i].sum;
+    for (let i = 0; i < materials.unconfirmed.length; i++) {
+      if (i === 0) {
+        reportSum = materials.unconfirmed[i].sum;
 
-				addReport();
-			} else {
-				if (materials.unconfirmed[i].report !== materials.unconfirmed[i - 1].report) {
-					unconfirmedReports[previousReport()] = { ...unconfirmedReports[previousReport()], reportSum };
+        addReport();
+      } else {
+        if (
+          materials.unconfirmed[i].report !==
+          materials.unconfirmed[i - 1].report
+        ) {
+          unconfirmedReports[previousReport()] = {
+            ...unconfirmedReports[previousReport()],
+            reportSum,
+          };
 
-					reportSum = materials.unconfirmed[i].sum;
+          reportSum = materials.unconfirmed[i].sum;
 
-					addReport();
-				} else {
-					reportSum += materials.unconfirmed[i].sum;
-				}
-			}
+          addReport();
+        } else {
+          reportSum += materials.unconfirmed[i].sum;
+        }
+      }
 
-			if (i === materials.unconfirmed.length - 1) unconfirmedReports[previousReport()] = { ...unconfirmedReports[previousReport()], reportSum };
+      if (i === materials.unconfirmed.length - 1)
+        unconfirmedReports[previousReport()] = {
+          ...unconfirmedReports[previousReport()],
+          reportSum,
+        };
 
-			function addReport() {
-				const report = materials.unconfirmed[i];
+      function addReport() {
+        const report = materials.unconfirmed[i];
 
-				unconfirmedReports.push({
-					reportNumber: report.report,
-					reportDate: report.date,
-					reportTime: report.time,
-					reportType: report.type,
-					reportLabel: report.label,
-				});
-			}
-		}
+        unconfirmedReports.push({
+          reportNumber: report.report,
+          reportDate: report.date,
+          reportTime: report.time,
+          reportType: report.type,
+          reportLabel: report.label,
+        });
+      }
+    }
 
-		setMaterialsReports({ ...materialsReports, unconfirmed: unconfirmedReports });
-	}, [materials]);
+    setMaterialsReports({
+      ...materialsReports,
+      unconfirmed: unconfirmedReports,
+    });
+  }, [materials]);
 
-	async function getMaterialsReport() {
-		dispatch(showProgress("Завантаження звітів..."));
+  async function getMaterialsReport() {
+    dispatch(showProgress("Завантаження звітів..."));
 
-		await fetch(REPORT_MATERIALS_URL)
-			.then(response => response.json())
-			.then(response => {
-				setMaterials({ ...materials, unconfirmed: response.data });
-				dispatch(hideProgress());
-			})
-			.catch(error => {
-				dispatch(hideProgress());
-				dispatch(showNotification(error));
+    await fetch(REPORT_UNC_MATERIALS_URL)
+      .then((response) => response.json())
+      .then((response) => {
+        setMaterials({ ...materials, unconfirmed: response.data });
+        dispatch(hideProgress());
+      })
+      .catch((error) => {
+        dispatch(hideProgress());
+        dispatch(showNotification(error));
 
-				xc.e(error);
-			});
-	}
+        xc.e(error);
+      });
+  }
 
-	return (
-		<div>
-			{materialsReports.unconfirmed[0]?.reportNumber && (
-				<div>
-					{/* <h1>{materialsReports.unconfirmed[0].reportNumber}</h1> */}
+  return (
+    <div>
+      {materialsReports.unconfirmed[0]?.reportNumber && (
+        <div>
+          <ul>
+            {materialsReports.unconfirmed.map((item) => {
+              return (
+                <li
+                  style={{
+                    display: "flex",
+                    width: "95%",
+                    gap: "20px",
+                    marginBottom: "20px",
+                    padding: "10px 0",
+                    borderTop: "2px solid #a55",
+                    borderBottom: "2px solid #a55",
+                  }}
+                  key={item.reportNumber}
+                >
+                  <div>
+                    Звіт <b>{item.reportNumber}</b> від {item.reportDate}{" "}
+                    {item.reportTime}
+                  </div>
 
-					<ul>
-						{materialsReports.unconfirmed.map(item => {
-							<li key={item.reportNumber}>
-								Report <b>{item.reportNumber}</b>
-							</li>;
-						})}
-					</ul>
-				</div>
-			)}
-		</div>
-	);
+                  <div>{item.reportType}</div>
+
+                  <div>
+                    Сума: <b>{item.reportSum} грн</b>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
